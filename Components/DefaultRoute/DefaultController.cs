a9ec97fe7c37ae541e6ec1f65aeb25919e413ad4
Manu Khullar <manu_khullar@hotmail.com>
2018-06-08 09:29:45 -0600

    secured default controller

--- Components/DefaultRoute/DefaultController.cs
+++ Components/DefaultRoute/DefaultController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DefaultRoute
 {
     [Route("")]
+    [Authorize(Policy = "pal-tracker")]
     public class DefaultController : Controller
     {
         [HttpGet]
